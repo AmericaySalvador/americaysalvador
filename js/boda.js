3bbbcc4f75fed2e96e5a8b0699cd39882ea531fd
AmericaySalvador <143547230+AmericaySalvador@users.noreply.github.com>
2023-09-08 12:12:43 -0600

    Update boda.js

--- js/boda.js
+++ js/boda.js
@@ -7,7 +7,7 @@ function sendWhatsapp() {
   if (document.getElementById("name").value == "") {
     Swal.fire({
       title: '',
-      html: '<img src="americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%;"><h2 style="color: black; font-family: \'Montserrat\', sans-serif;">Ingresa tu nombre</h2>',
+      html: '<img src="https://americaysalvador.github.io/americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%;"><h2 style="color: black; font-family: \'Montserrat\', sans-serif;">Ingresa tu nombre</h2>',
       background: 'rgba( 251, 250, 249,30.1)',
       confirmButtonText: 'Ok',
       confirmButtonColor: '#000000',
@@ -18,7 +18,7 @@ function sendWhatsapp() {
   if (document.getElementById("email").value == "") {
     Swal.fire({
       title: '',
-      html: '<img src="americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%; "><h2 style="color: black;font-family: \'Montserrat\', sans-serif; ">Ingresa tu correo</h2>',
+      html: '<img src="https://americaysalvador.github.io/americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%; "><h2 style="color: black;font-family: \'Montserrat\', sans-serif; ">Ingresa tu correo</h2>',
       background: 'rgba( 251, 250, 249,30.1)',
       confirmButtonText: 'Ok',
       confirmButtonColor: '#000000',
@@ -29,7 +29,7 @@ function sendWhatsapp() {
   if (document.getElementById("phone").value == "") {
     Swal.fire({
       title: '',
-      html: '<img src="americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%;"><h2 style="color: black;font-family: \'Montserrat\', sans-serif; ">Ingresa tu Numero de Telefono</h2>',
+      html: '<img src="https://americaysalvador.github.io/americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%;"><h2 style="color: black;font-family: \'Montserrat\', sans-serif; ">Ingresa tu Numero de Telefono</h2>',
       background: 'rgba( 251, 250, 249,30.1)',
       confirmButtonText: 'Ok',
       confirmButtonColor: '#000000',
@@ -40,7 +40,7 @@ function sendWhatsapp() {
   if (document.getElementById("guest").value == "") {
     Swal.fire({
       title: '',
-      html: '<img src="americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%;"><h2 style="color: black; font-family: \'Montserrat\', sans-serif; ">Parece que no estas invitado</h2>',
+      html: '<img src="https://americaysalvador.github.io/americaysalvador/image/ays/favorites/s.jpg" style="width: 50%; height: 70%;"><h2 style="color: black; font-family: \'Montserrat\', sans-serif; ">Parece que no estas invitado</h2>',
       background: 'rgba( 251, 250, 249,30.1)',
       confirmButtonText: 'Ok',
       confirmButtonColor: '#000000',
